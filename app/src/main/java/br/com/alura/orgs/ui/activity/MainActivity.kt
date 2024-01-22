b202037392be7df28bebf4c5a765b8e90fbf65e6
Giovanni Zandonai <giovannizandonai350@gmail.com>
2024-01-22 13:23:21 -0300

    floating button

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,25 +1,24 @@
 package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
 import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.common.collect.ForwardingMultimap
 import java.math.BigDecimal
+import java.text.Normalizer.Form
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta de frutas"
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Laranja, manga e maçã"
-//        val valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "19.99"
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
                 Produto(nome = "teste",
@@ -35,7 +34,12 @@ class MainActivity : Activity() {
                         valor = BigDecimal("39.99")
                 ),
         ))
-//        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fabButton_add)
+       fab.setOnClickListener{
+            val intent = Intent(this, FormProdutoActivity::class.java)
+            startActivity(intent)
+       }
     }
 
 }
